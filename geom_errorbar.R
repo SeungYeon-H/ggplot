6c4f55bbda952d542248e4a4f5305683f0a00491
Seungyeon <155028735+SeungYeon-H@users.noreply.github.com>
2024-02-05 09:22:26 -0500

    Update geom_errorbar.R

--- geom_errorbar.R
+++ geom_errorbar.R
@@ -1,4 +1,4 @@
-####using geom_errobar to indicate max-min range (periodical range) in R
+####using geom_errorbar to indicate max-min range 
 
 sample<-read_xlsx("dates.xlsx",sheet="Sheet1")`
 
@@ -26,14 +26,20 @@ sample$min<- as.numeric(min)
 sample$average<- as.numeric(average)
 sample<-sample[order(sample$year),]
 
-prac<-ggplot(sample, aes(x=reorder(site, year), y=average,ymin = min, ymax = max,color=type))+
+prac<-ggplot(sample, aes(x=reorder(site, year), 
+                         y=average,ymin = min, ymax = max,color=type))+
   geom_hline(yintercept=c(926,946),linetype=1,size=2,color="orange",alpha=0.6)+ 
-  geom_errorbar(size=1.2,width=0.3,alpha=.8)+geom_point(size=4)+
+  geom_errorbar(size=1.2,width=0.3,alpha=.8)+
+  geom_point(size=4)+
   coord_flip()+  
   scale_color_manual(values=c("#2F4E62","#6C8983","#61645B"))+ 
   theme_bw()+
   labs(y="Calendar year (AD)")+
-  scale_y_continuous(breaks=seq(760,1020,20),sec.axis = dup_axis())+  
-  theme(axis.title.y=element_blank(),axis.title.x=element_text(face="bold",size=15),axis.text.x=element_text(face="bold",size=11),legend.title=element_blank(),legend.position="none",axis.text.y=element_text(face="bold",size=12))
+  scale_y_continuous(breaks=seq(760,1020,20),
+                     sec.axis = dup_axis())+  
+  theme(axis.title.y=element_blank(),axis.title.x=element_text(face="bold",size=15),
+        axis.text.x=element_text(face="bold",size=11),legend.title=element_blank(),
+        legend.position="none",axis.text.y=element_text(face="bold",size=12)
+       )
 
 prac
